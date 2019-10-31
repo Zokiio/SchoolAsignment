af87d6f1ae99dfa2514d8b8085be485167cc467c
Nayomi <nayomi.arvell@gmail.com>
2019-10-31 13:12:13 +0100

    addsDataTableUnderSearchbar

--- react-app/src/Data/GetFunctions.js
+++ react-app/src/Data/GetFunctions.js
@@ -1,22 +1,22 @@
 function GetAll() {
-  return fetch('http://localhost:51592/').then(result => result.json());
+  return fetch('http://localhost:1065/').then(result => result.json());
 }
 
 function GetCountry(country) {
-  return fetch(`http://localhost:51592/${country}/`).then(result =>
+  return fetch(`http://localhost:1065/${country}/`).then(result =>
     result.json()
   );
 }
 
 function GetDepartment(country, department) {
-  return fetch(`http://localhost:51592/${country}/${department}/`).then(
+  return fetch(`http://localhost:1065/${country}/${department}/`).then(
     result => result.json()
   );
 }
 
 function GetEmployee(country, department, employee) {
   return fetch(
-    `http://localhost:51592/${country}/${department}/${employee}`
+    `http://localhost:1065/${country}/${department}/${employee}`
   ).then(result => result.json());
 }
 
