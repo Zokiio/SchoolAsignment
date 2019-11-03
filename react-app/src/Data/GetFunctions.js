068ed2383d7f1ce97e00789147ad47aa315421fe
joakim.hall1@gmail.com <joakim.hall1@gmail.com>
2019-11-03 13:57:02 +0100

    Add port varible for easy change

--- react-app/src/Data/GetFunctions.js
+++ react-app/src/Data/GetFunctions.js
@@ -1,25 +1,25 @@
-const port = 1065;
+const port = 51592;
 
 function GetAll() {
-  return fetch('http://localhost:19505/').then(result => result.json());
+  return fetch(`http://localhost:${port}`).then(result => result.json());
 }
 
 function GetCountry(country) {
-  return fetch(`http://localhost:19505/${country}/`).then(result =>
+  return fetch(`http://localhost:${port}/${country}/`).then(result =>
     result.json()
   );
 }
 
 function GetDepartment(country, department) {
-  return fetch(`http://localhost:19505/${country}/${department}/`).then(
+  return fetch(`http://localhost:${port}/${country}/${department}/`).then(
     result => result.json()
   );
 }
 
 function GetEmployee(country, department, employee) {
-  return fetch(
-    `http://localhost:${port}/19505/${department}/${employee}`
-  ).then(result => result.json());
+  return fetch(`http://localhost:${port}/${department}/${employee}`).then(
+    result => result.json()
+  );
 }
 
 export { GetAll, GetCountry, GetDepartment, GetEmployee };
